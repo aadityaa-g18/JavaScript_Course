ba60d9f73981fc4fb05c96d70a5b7522599fbaef
Aditya <105495718+aadityaa-g18@users.noreply.github.com>
2025-07-15 09:57:34 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict";  //treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+let name = "Aditya"
+let age = 22
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => satndalone value --> representing empty value
+// undefined => value not assigned yet
+// symbol => unique
+
+
+// object
+
+console.log(typeof "Aditya");
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
